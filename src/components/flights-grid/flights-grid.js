a1ff14034ad16f23584129c93151154ae4594f5a
Santosh <m.santosh33@gmail.com>
2022-07-11 20:14:25 +0100

    added sort icons

--- src/components/flights-grid/flights-grid.js
+++ src/components/flights-grid/flights-grid.js
@@ -4,8 +4,6 @@ import "../flight-info/flight-info.css";
 import { FlightSearchInfo } from "./../flight-search-info/flight-search-info";
 import { FlightInfo } from "./../flight-info/flight-info";
 import { MultiFlightInfo } from "./../multi-flight-info/multi-flight-info";
-import { Card } from "react-bootstrap";
-import { DetailLabel } from "../detail-label/detail-label";
 
 const FlightsGrid = (props) => {
   const flights = props.flights || {};
@@ -36,31 +34,76 @@ const FlightsGrid = (props) => {
       {props.criteria && (
         <FlightSearchInfo criteria={props.criteria} count={flightsCount || 0} />
       )}
-      <Card>
-        <section className="Flight-info">
-          <DetailLabel mainText={""} subText={""}></DetailLabel>
-          <DetailLabel mainText={""} subText={"Airlines"}></DetailLabel>
-          <DetailLabel mainText={""} subText={"Departure"}></DetailLabel>
-          <DetailLabel mainText={""} subText={"Arrival"}></DetailLabel>
-          <DetailLabel mainText={""} subText={"Duration"}></DetailLabel>
-          <DetailLabel mainText={""} subText={"Price"}></DetailLabel>
+      <div class="card">
+        <section class="Flight-info ">
+          <img
+            style={{ visibility: "hidden" }}
+            src="/static/media/nonstop.4edbe6dd4c40148091d6.png"
+            alt="direct flight logo"
+            width="32"
+            height="32"
+          />
+          <div class="detail-label">
+            <h4>Airlines</h4>
+          </div>
+          <div class="detail-label">
+            <h4>Departure Time</h4>
+          </div>
+          <div class="detail-label">
+            <h4>Arrival Time</h4>
+          </div>
+          <div class="detail-label duration-header">
+            <h4>
+              Duration
+              <span className="sort-icon">
+                <a
+                  href="#"
+                  onClick={(event) => {
+                    event.preventDefault();
+                    flightSort("price", true);
+                  }}>
+                  <i className="fa fa-fw fa-sort-asc"></i>
+                </a>
+                <a
+                  href="#"
+                  onClick={(event) => {
+                    event.preventDefault();
+                    flightSort("price", false);
+                  }}>
+                  <i className="fa fa-fw fa-sort-desc"></i>
+                </a>
+              </span>
+            </h4>
+          </div>
+          <p className="price-header">
+            Price{" "}
+            <span className="sort-icon">
+              <a
+                href="#"
+                onClick={(event) => {
+                  event.preventDefault();
+                  flightSort("price", true);
+                }}>
+                <i className="fa fa-fw fa-sort-asc"></i>
+              </a>
+              <a
+                href="#"
+                onClick={(event) => {
+                  event.preventDefault();
+                  flightSort("price", false);
+                }}>
+                <i className="fa fa-fw fa-sort-desc"></i>
+              </a>{" "}
+            </span>
+          </p>
           <button
-            onClick={() => {
-              flightSort("price", true);
-            }}
-          >
-            Up
+            type="button"
+            style={{ visibility: "hidden" }}
+            class="btn btn-primary">
+            Select
           </button>
-          <button
-            onClick={() => {
-              flightSort("price", false);
-            }}
-          >
-            Down
-          </button>
-          <DetailLabel mainText={""} subText={""}></DetailLabel>
         </section>
-      </Card>
+      </div>
       {sortedData.nonStopFlights &&
         sortedData.nonStopFlights.map((flight) => <FlightInfo data={flight} />)}
       {sortedData.multiStopFlights &&
